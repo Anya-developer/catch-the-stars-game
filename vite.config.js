b7d475e4cb4aef89a18f507cb345922ef243af1a
Anya-developer <anna.coder316@gmail.com>
2025-09-20 20:07:48 +0300

    configure the project build

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,5 @@
-// import { defineConfig } from "vite";
-// import pugPlugin from "vite-plugin-pug";
-// import { resolve } from 'path'
-// const options = { pretty: true };
-// const locals = { name: "My Pug" };
-// import { viteStaticCopy } from "vite-plugin-static-copy";
-
-
-// export default defineConfig({
-//   root: "./",
-//   plugins: [
-//     pugPlugin({ localImports: true }, { hello: "world", baseUrl: "./src" }),
-//     viteStaticCopy({
-//       targets: [
-//         {
-//           src: "src/audio", // копируем папку audio
-//           dest: "."         // положим её прямо в dist/
-//         }
-//       ]
-//     })
-//   ],
-//   base: './',
-
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         api: 'modern-compiler',
-//       }
-//     }
-//   },
-
-//   server: {
-//     watch: {
-//       usePolling: true
-//     }
-//   },
-  
-//   build: {
-//     outDir: './dist',
-//     emptyOutDir: true,
-//   } 
-// })
-
 import { defineConfig } from "vite";
 import pugTransformer from "vite-plugin-pug-transformer";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import sassGlobImports from 'vite-plugin-sass-glob-import'
 
 export default defineConfig({
