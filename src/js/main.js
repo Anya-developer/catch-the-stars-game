4fbd9380962cf9f7a14fa9f9cb9bc8720cf85de4
Anya-developer <anna.coder316@gmail.com>
2025-09-20 21:53:07 +0300

    the final project

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import {spaceShipMove} from "./components/space-ship-move.js";
 import {starsFall} from "./components/stars-fall.js";
 import {starsShine} from "./components/stars-shine.js";
 import {shipFly} from "./components/ship-fly.js";
-import '../styles/style.scss';
+import '/styles/style.scss';
 
 document.addEventListener('DOMContentLoaded', function () {
   commonFunction()
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
+import path, { resolve } from 'node:path';
+import url from 'node:url';
 import { defineConfig } from "vite";
 import pugTransformer from "vite-plugin-pug-transformer";
 import sassGlobImports from 'vite-plugin-sass-glob-import'
 
+const root = resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'src')
+const outDir = resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'dist')
+
 export default defineConfig({
-  root: "src",
+  root,
   build: {
-    outDir: "../dist",
+    outDir,
     emptyOutDir: true,
     rollupOptions: {
-      input: "./src/index.html",
       output: {
         assetFileNames: assetInfo => {
           let extType = assetInfo.name.split('.')[1]
